62381912d2f3bf5e64b8d7e4d30608c48eb14891
ckyever <ckyeverg@gmail.com>
2025-10-01 17:54:30 +1000

    Add list module

---
+++ src/list.js
@@ -0,0 +1,20 @@
+export default class List {
+  #todos;
+
+  constructor(title) {
+    this.title = title;
+    this.#todos = [];
+  }
+
+  addTodo(todo) {
+    this.#todos.append(todo);
+  }
+
+  deleteTodo(todo) {
+    const index = this.#todos.indexOf(todo);
+
+    if (index >= 0) {
+      this.#todos.splice(index, 1);
+    }
+  }
+}
