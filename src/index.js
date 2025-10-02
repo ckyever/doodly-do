59b250de5dca2f12232ba566cc5fc16bfdbd659c
ckyever <ckyeverg@gmail.com>
2025-10-02 14:12:42 +1000

    Add a dialog to create a list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./styles.css";
+import listService from "./listService.js";
---
+++ src/listService.js
@@ -0,0 +1,33 @@
+import List from "./List.js";
+
+class ListService {
+  constructor() {
+    this.lists = [];
+
+    const createListButton = document.querySelector("button.create-list");
+    const newListDialog = document.querySelector("dialog.list-creator");
+    const newListForm = newListDialog.querySelector("form.new-list");
+    const newListTitle = newListForm.querySelector("input.title");
+    const newListCancel = newListForm.querySelector("button.cancel");
+
+    createListButton.addEventListener("click", () => {
+      newListDialog.showModal();
+    });
+
+    newListCancel.addEventListener("click", () => {
+      newListForm.reset();
+      newListDialog.close();
+    });
+
+    newListForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const newList = new List(newListTitle.value);
+      this.lists.push(newList);
+      newListForm.reset();
+      newListDialog.close();
+      console.table(this.lists); // CKYTODO Remove
+    });
+  }
+}
+
+export default new ListService();
