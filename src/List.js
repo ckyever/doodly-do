ce831ebb3c4303283fc49eb058147b175848f3b4
ckyever <ckyeverg@gmail.com>
2025-10-04 20:16:15 +1000

    Remove private variables from Class objects
    This is so we can save the properties to local storage

--- src/List.js
+++ src/List.js
@@ -1,30 +1,19 @@
 export default class List {
-  #todos;
-  #id;
-
   constructor(title) {
     this.title = title;
-    this.#todos = [];
-    this.#id = crypto.randomUUID();
-  }
-
-  get id() {
-    return this.#id;
-  }
-
-  get todos() {
-    return this.#todos;
+    this.todos = [];
+    this.id = crypto.randomUUID();
   }
 
   addTodo(todo) {
-    this.#todos.push(todo);
+    this.todos.push(todo);
   }
 
   deleteTodo(todo) {
-    const index = this.#todos.indexOf(todo);
+    const index = this.todos.indexOf(todo);
 
     if (index >= 0) {
-      this.#todos.splice(index, 1);
+      this.todos.splice(index, 1);
     }
   }
 };
--- src/Todo.js
+++ src/Todo.js
@@ -1,20 +1,13 @@
 export default class Todo {
-  #id;
-
   constructor(title = "", details = {}) {
     this.title = title;
     this.description = details.description ?? null;
     this.dueDate = details.dueDate ?? null;
     this.isComplete = false;
-    this.#id = crypto.randomUUID();
+    this.id = crypto.randomUUID();
   }
 
   toggle() {
     this.isComplete = !this.isComplete;
   }
-
-  get id() {
-    return this.#id;
-  }
-
 };
